07c4be6311f76d7b5d0c0727ec279260471595ef
garritfra <garritfranke@gmail.com>
2019-06-27 22:10:08 +0200

    Adjust debug output

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 const { parse } = require("./parsing");
 import fs from "fs";
 import Tokenizer from "./Tokenizer";
-import { TokenType } from "./tokens";
 
 let fileString = fs.readFileSync("./test.flex").toString();
 
 let tokenizer = new Tokenizer(fileString);
 
 let ast = tokenizer.run();
-ast.forEach(token => console.log(TokenType[token.type], token.value));
+console.log(ast);
