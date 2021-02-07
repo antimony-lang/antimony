14e0f07f2d5eaa6996a73f8304bb38391d59242b
Garrit Franke <garrit@slashdev.space>
2021-02-07 16:48:16 +0100

    docs: fix build script

--- build_docs.py
+++ build_docs.py
@@ -1,13 +1,14 @@
 import subprocess
 import os
+import time
 
 os.system("mdbook build --dest-dir './book'")
 os.system("mdbook build --dest-dir './book/latest'")
 tags = subprocess.run(["git", "tag"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-
 for tag in tags.split("\n"):
     if tag == "":
         continue
     print("Building tag", tag)
-    os.system("git switch " + tag)
+    os.system("git checkout " + tag)
+    time.sleep(1)
     os.system("mdbook build --dest-dir './book/" + tag + "'")
