3fd609862742b6712c2576a52a7a3cd01f1c9ac9
Garrit Franke <garrit@slashdev.space>
2021-02-20 14:17:49 +0100

    feat: nested structs

--- src/parser/rules.rs
+++ src/parser/rules.rs
@@ -341,7 +341,12 @@ impl Parser {
     fn parse_field_access(&mut self, lhs: Expression) -> Result<Expression, String> {
         self.match_token(TokenKind::Dot)?;
         let field = self.match_identifier()?;
-        Ok(Expression::FieldAccess(Box::new(lhs), field))
+        let expr = Expression::FieldAccess(Box::new(lhs), field);
+        if self.peek_token(TokenKind::Dot).is_ok() {
+            self.parse_field_access(expr)
+        } else {
+           Ok(expr) 
+        }
     }
 
     /// TODO: Cleanup
