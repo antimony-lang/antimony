9f54a4d1ab48aa076b4c2d382253e3adeb9816f4
Alexey Yerin <yyp@disroot.org>
2021-02-27 20:15:27 +0300

    qbe: move helpers to QBEGenerator struct

--- src/generator/qbe.rs
+++ src/generator/qbe.rs
@@ -1,69 +1,74 @@
 use super::Generator;
-use crate::ast::{
-    Module,
-    Variable,
-    Function,
-};
 use crate::ast::types::Type;
+use crate::ast::{Function, Module, Variable};
 
-pub struct QBEGenerator;
+pub struct QBEGenerator {
+    pub code: String,
+}
 
 impl Generator for QBEGenerator {
     fn generate(prog: Module) -> String {
-        let mut code = String::new();
+        let mut gen = QBEGenerator::new();
 
         for func in prog.func.iter() {
-            code.push_str(&generate_function(func))
+            gen.add_function(func)
         }
 
-        code
+        gen.code
     }
 }
 
-fn generate_type(ty: &Type) -> String {
-    match ty {
-        // TODO: differentiate between 32- and 64-bit values
-        Type::Int => "w".into(),
-        _ => todo!(),
+impl QBEGenerator {
+    pub(crate) fn new() -> Self {
+        Self {
+            code: String::new(),
+        }
     }
-}
 
-fn generate_function_params(arguments: &Vec<Variable>) -> String {
-    let mut buf = String::new();
+    fn generate_type(ty: &Type) -> String {
+        match ty {
+            // TODO: differentiate between 32- and 64-bit values
+            Type::Int => "w".into(),
+            _ => todo!(),
+        }
+    }
 
-    let len = arguments.len();
-    for (i, arg) in arguments.into_iter().enumerate() {
-        buf.push_str(
-            &format!(
+    fn generate_function_params(arguments: &Vec<Variable>) -> String {
+        let mut buf = String::new();
+
+        let len = arguments.len();
+        for (i, arg) in arguments.into_iter().enumerate() {
+            buf.push_str(&format!(
                 "{type} %{ident}",
                 // Types for parameters are required
-                type = generate_type(arg.ty.as_ref().unwrap()),
+                type = Self::generate_type(arg.ty.as_ref().unwrap()),
                 ident = arg.name,
-            )
-        );
+            ));
 
-        if i < len - 1 {
-            buf.push_str(", ");
+            if i < len - 1 {
+                buf.push_str(", ");
+            }
         }
+
+        buf
     }
 
-    buf
-}
+    fn add_function(&mut self, func: &Function) {
+        let return_type = Self::generate_type(&func.ret_type.as_ref().unwrap_or(&Type::Int));
+        let params = Self::generate_function_params(&func.arguments);
 
-fn generate_function(func: &Function) -> String {
-    let return_type = generate_type(&func.ret_type.as_ref().unwrap_or(&Type::Int));
-    let params = generate_function_params(&func.arguments);
-
-    // XXX: Do we need to export all functions? Or when noted as so?
-    // TODO: We might get a function collision. Or not?
-    format!("export function {return_type} ${name}({params}) {{
+        // XXX: Do we need to export all functions? Or when noted as so?
+        // TODO: We might get a function collision. Or not?
+        self.code.push_str(&format!(
+            "export function {return_type} ${name}({params}) {{
 @start
 {instructions}
 }}
 ",
-    return_type = return_type,
-    name = func.name,
-    params = params,
-    instructions = ""
-    )
+            return_type = return_type,
+            name = func.name,
+            params = params,
+            instructions = ""
+        ));
+    }
 }
