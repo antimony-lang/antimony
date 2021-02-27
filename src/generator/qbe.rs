18eedbc5f1e1c35e5f33499fdd8afb990a7a729e
Alexey Yerin <yyp@disroot.org>
2021-02-27 20:20:02 +0300

    qbe: make function generation more readable

--- src/generator/qbe.rs
+++ src/generator/qbe.rs
@@ -53,22 +53,30 @@ impl QBEGenerator {
         buf
     }
 
+    /// Adds a block label to the generated code
+    fn add_block_label(&mut self, name: &str) {
+        self.code.push_str(&format!("@{}", name));
+    }
+
     fn add_function(&mut self, func: &Function) {
         let return_type = Self::generate_type(&func.ret_type.as_ref().unwrap_or(&Type::Int));
         let params = Self::generate_function_params(&func.arguments);
 
         // XXX: Do we need to export all functions? Or when noted as so?
         // TODO: We might get a function collision. Or not?
+
+        // export function w $myfunc(w %age) {
         self.code.push_str(&format!(
-            "export function {return_type} ${name}({params}) {{
-@start
-{instructions}
-}}
-",
+            "export function {return_type} ${name}({params}) {{",
             return_type = return_type,
             name = func.name,
             params = params,
-            instructions = ""
         ));
+
+        // @start
+        self.add_block_label("start");
+
+        // }
+        self.code.push_str("}\n");
     }
 }
