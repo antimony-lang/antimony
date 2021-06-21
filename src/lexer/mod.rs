7a3c2e39dde9231e45be67f863e9bc08fd267da6
Garrit Franke <garrit@slashdev.space>
2021-06-21 12:11:49 +0200

    lex: check for unicode whitespace

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -178,14 +178,29 @@ pub fn first_token(input: &str, pos: &mut Position) -> Token {
 }
 
 pub fn is_whitespace(c: char) -> bool {
-    match c {
-        ' ' | '\n' | '\r' | '\t' => true,
-        '\u{00A0}' => {
-            dbg!("Non-standard unicode character found: '\u{00A0}'");
-            true
-        }
-        _ => false,
-    }
+
+    // https://doc.rust-lang.org/reference/whitespace.html
+    matches!(
+        c,
+        // Usual ASCII suspects
+        '\u{0009}'   // \t
+        | '\u{000A}' // \n
+        | '\u{000B}' // vertical tab
+        | '\u{000C}' // form feed
+        | '\u{000D}' // \r
+        | '\u{0020}' // space
+
+        // NEXT LINE from latin1
+        | '\u{0085}'
+
+        // Bidi markers
+        | '\u{200E}' // LEFT-TO-RIGHT MARK
+        | '\u{200F}' // RIGHT-TO-LEFT MARK
+
+        // Dedicated whitespace characters from Unicode
+        | '\u{2028}' // LINE SEPARATOR
+        | '\u{2029}' // PARAGRAPH SEPARATOR
+    )
 }
 
 /// True if `c` is valid as a first character of an identifier.
