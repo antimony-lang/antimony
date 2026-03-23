ea70e70ba1ed9a3bff7eec2f81a0296162859b69
garritfra <32395585+garritfra@users.noreply.github.com>
2026-03-23 08:13:30 +0000

    Deploying to gh-pages from @ antimony-lang/antimony@6fa6feced8585f5cbadd19c99d95120c68c8b92a 🚀

--- latest/searcher-c2a407aa.js
+++ latest/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-11993848.js',
+                path_to_root + 'searchindex-d2c2f215.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
